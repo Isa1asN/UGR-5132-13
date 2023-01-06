557a24f822b169918547d8ab5e912e73df0a92f9
Esayas Nigussie <101455473+Isa1asN@users.noreply.github.com>
2023-01-07 00:32:19 +0300

    Create Home_script.js

---
+++ src/script/Home_script.js
@@ -0,0 +1,33 @@
+
+// -------------------------This is for the email subscription--------------
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz2q9L1X_hKlbuKN39D0Wg9uCeJ1JbjrRSgtkI1D2xcGvr-zMX_ehX7MZ7RbW7CtGYJ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const input = document.getElementById('emailform')
+  const msg = document.getElementById('enter_email')
+  const btn = document.getElementById('subscribe')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        input.readOnly = true;
+        msg.innerText = "Thanks for subscribing!"
+        msg.style = 'color: red;'
+        setTimeout(function(){
+          btn.style = 'display:none;'
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+// _----____-----____-----_____------______________-----------------
+// ______________________________NAV BAR_______________________________-
+
+const close = document.getElementById('close_btn')
+const menu = document.getElementById('menu_btn')
+close.addEventListener('click', ()=>{
+  document.querySelector('nav .navigation ul').classList.remove('active')
+})
+menu.addEventListener('click',()=>{
+  document.querySelector('nav .navigation ul').classList.add('active')
+})
