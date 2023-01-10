dae9ee662488a146696bc3efcde44c29d22555b8
Esayas Nigussie <101455473+Isa1asN@users.noreply.github.com>
2023-01-10 20:25:50 +0300

    Update Contact_script.js

--- src/script/Contact_script.js
+++ src/script/Contact_script.js
@@ -3,7 +3,7 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbymARiZlAu1kHvVuCKtv3
 const form = document.forms['contact-list']
 const btn = document.getElementById('btn-submit').addEventListener('click',()=>{
   btn.style.display = 'none'
-  confirm("Message sent succesfully, we'll be in touch soon."
+  confirm("Message sent succesfully, we'll be in touch soon.")
 })
 
 
