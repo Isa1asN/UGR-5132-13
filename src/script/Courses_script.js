d0c774db6433c95be698029f38c2399deb9f15f7
Esayas Nigussie <101455473+Isa1asN@users.noreply.github.com>
2023-01-10 16:07:40 +0300

    Update Courses_script.js

--- src/script/Courses_script.js
+++ src/script/Courses_script.js
@@ -7,15 +7,6 @@ menu.addEventListener('click',()=>{
   document.querySelector('nav .navigation ul').classList.add('active')
 })
 
-const close = document.getElementById('close_btn')
-const menu = document.getElementById('menu_btn')
-close.addEventListener('click', ()=>{
-  document.querySelector('nav .navigation ul').classList.remove('active')
-})
-menu.addEventListener('click',()=>{
-  document.querySelector('nav .navigation ul').classList.add('active')
-})
-
 // _____________________________________________________________________
 
 function search() {
