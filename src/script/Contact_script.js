143effc5a1b1ede6ee0e499aa98abda4d2c25926
Esayas Nigussie <101455473+Isa1asN@users.noreply.github.com>
2023-01-10 20:13:59 +0300

    Update Contact_script.js

--- src/script/Contact_script.js
+++ src/script/Contact_script.js
@@ -1,3 +1,22 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbymARiZlAu1kHvVuCKtv39dloWLFyHk-mjUAo3GN78-psRxwnoC1OZMcUmwLWNu4yRS9Q/exec'
+
+const form = document.forms['contact-list']
+const btn = document.getElementById('btn-submit')
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      console.log('Success!', response)
+      btn.style.display = 'none'
+      confirm("Message sent succesfully, thanks we'll be in touch.")
+
+  })
+
+    .catch(error => console.error('Error!', error.message))
+})
+// _______________________________________navbar_________________________________________________
 const close = document.getElementById('close_btn')
 const menu = document.getElementById('menu_btn')
 close.addEventListener('click', ()=>{
