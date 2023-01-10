bbfc755f1ed1fe8747fd33ca5c0129025b78d027
Esayas Nigussie <101455473+Isa1asN@users.noreply.github.com>
2023-01-10 15:34:59 +0300

    Update Courses_script.js

--- src/script/Courses_script.js
+++ src/script/Courses_script.js
@@ -6,3 +6,38 @@ close.addEventListener('click', ()=>{
 menu.addEventListener('click',()=>{
   document.querySelector('nav .navigation ul').classList.add('active')
 })
+
+const close = document.getElementById('close_btn')
+const menu = document.getElementById('menu_btn')
+close.addEventListener('click', ()=>{
+  document.querySelector('nav .navigation ul').classList.remove('active')
+})
+menu.addEventListener('click',()=>{
+  document.querySelector('nav .navigation ul').classList.add('active')
+})
+
+// _____________________________________________________________________
+
+function search() {
+ let input = document.getElementById('search').value
+ input = input.toLowerCase()
+ let x = document.getElementsByClassName('card-text');
+
+ if (input == ""){
+ for (i = 0; i < x.length; i++) { 
+  x[i].parentElement.parentElement.parentElement.style.display="initial";                
+ }
+ }
+ 
+
+ for (i = 0; i < x.length; i++) { 
+  if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].parentElement.parentElement.parentElement.style.display="none";
+  }
+  else {
+      x[i].style.display="initial";                 
+  }
+}
+}
+
+let input = document.getElementById('search').addEventListener('keyup', search)
