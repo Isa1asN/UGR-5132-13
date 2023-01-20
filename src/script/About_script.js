dd3710e4f6ec5a79177f27efe7165001f795b248
Esayas Nigussie <101455473+Isa1asN@users.noreply.github.com>
2023-01-20 12:13:57 +0300

    Delete About_script.js

--- src/script/About_script.js
+++
@@ -1,8 +0,0 @@
-const close = document.getElementById('close_btn')
-const menu = document.getElementById('menu_btn')
-close.addEventListener('click', ()=>{
-  document.querySelector('nav .navigation ul').classList.remove('active')
-})
-menu.addEventListener('click',()=>{
-  document.querySelector('nav .navigation ul').classList.add('active')
-})
